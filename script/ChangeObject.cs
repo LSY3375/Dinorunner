00a2f184327160a98f7f1186e500940073a2c6ca
LSY <105796523+LSY3375@users.noreply.github.com>
2024-11-16 17:48:44 +0900

    Create ChangeObject.cs

---
+++ script/ChangeObject.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeObject : MonoBehaviour
+{
+    public GameObject[] objs;
+
+    //선인장 교체
+    public void Change()
+    {
+        int ran = Random.Range(0, objs.Length);
+
+        for (int index = 0; index < objs.Length; index++)
+        {
+            transform.GetChild(index).gameObject.SetActive(ran == index);
+        }
+    }
+}
